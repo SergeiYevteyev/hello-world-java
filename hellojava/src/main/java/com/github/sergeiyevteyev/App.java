cc1b00791ffdbbf89ad0abba2f4f48bfd911ebf4
Sergejs Jevtejevs <ext-sergejs.jevtejevs@intrum.com>
2020-02-28 17:27:54 +0200

    added jdbc for mssql with open connection

--- hellojava/src/main/java/com/github/sergeiyevteyev/App.java
+++ hellojava/src/main/java/com/github/sergeiyevteyev/App.java
@@ -1,5 +1,7 @@
 package com.github.sergeiyevteyev;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -17,5 +19,18 @@ public class App
         System.out.println(LocalDate.now());
         System.out.println(LocalDateTime.now());
         System.out.println(LocalTime.now());
+
+        String connectionUrl = "jdbc:sqlserver://:1433;databaseName=sls;user=;password=;";
+
+        try {
+            // Load SQL Server JDBC driver and establish connection.
+            System.out.println("Connecting to SQL Server ... ");            
+            try (Connection connection = DriverManager.getConnection(connectionUrl)) {
+                System.out.println("Done.");
+            }
+        } catch (Exception e) {
+            System.out.println();
+            e.printStackTrace();
+        }
     }
 }
