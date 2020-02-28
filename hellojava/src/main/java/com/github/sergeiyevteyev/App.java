9d7b784818e280ce6db9ba96094533fdc6c4b9fb
Sergejs Jevtejevs <ext-sergejs.jevtejevs@intrum.com>
2020-02-28 10:58:14 +0200

    maven project added

---
+++ hellojava/src/main/java/com/github/sergeiyevteyev/App.java
@@ -0,0 +1,17 @@
+package com.github.sergeiyevteyev;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
